666624fb27839666ff9ef7eb5716fdabefeea543
Nicholas Anna <NAnna@carbonite.com>
2019-03-01 17:21:56 -0500

    Takes an input date. Returns true if date during DST observation in USA

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-module.exports = (date) => {
-  if (typeof date === 'undefined') { date = new Date() }
-  if (!(date instanceof Date)) { throw new TypeError(`${typeof date} not supported. Expected Date or undefined.`) }
-
-  const january = new Date(date.getFullYear(), 0, 1)
-  const july = new Date(date.getFullYear(), 6, 1)
-
-  const januaryTimezoneOffset = january.getTimezoneOffset()
-  const julyTimezoneOffset = july.getTimezoneOffset()
-  const timezoneOffset = date.getTimezoneOffset()
-
-  if (timezoneOffset === januaryTimezoneOffset && timezoneOffset === julyTimezoneOffset) {
-    return false
+module.exports = exports = (date) => {
+  if (typeof date === 'undefined') {
+    date = new Date();
+  } else if (!(date instanceof Date)) {
+    throw new TypeError(`${typeof date} not supported. Expected Date or undefined.`);
   }
 
-  const standardTimezoneOffset = Math.max(januaryTimezoneOffset, julyTimezoneOffset)
-  return timezoneOffset < standardTimezoneOffset
+  const currentDate = date.getDate();
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+
+  const standardMonths = [0, 1, 11];
+  const savingMonths = [3, 4, 5, 6, 7, 8, 9];
+
+  if (standardMonths.includes(currentMonth)) {
+    return false;
+  } else if (savingMonths.includes(currentMonth)) {
+    return true;
+  } else if (currentMonth == 2) {
+    let sundayCounter = 0;
+    let changeoverDate = 0;
+    while (sundayCounter < 2) {
+      changeoverDate++;
+      let day = (new Date(currentYear, currentMonth, changeoverDate)).getDay();
+      if (day == 0) {
+        sundayCounter++;
+      }
+    }
+    if (currentDate < changeoverDate) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    let sundayCounter = 0;
+    let changeoverDate = 0;
+    while (sundayCounter < 1) {
+      changeoverDate++;
+      let day = (new Date(currentYear, currentMonth, changeoverDate)).getDay();
+      if (day == 0) {
+        sundayCounter++;
+      }
+    }
+    if (currentDate < changeoverDate) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
